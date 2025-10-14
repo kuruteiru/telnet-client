0b6efd3599beb78b4b56b62a8a1786f666d491ed
kuruteiru <martin.krutil@email.cz>
2025-10-14 15:48:34 +0200

    done, passing

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -2,10 +2,22 @@ package utb.fai;
 
 public class App {
 
-    public static void main(String[] args) {
-        // TODO: Implement input parameter processing
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.err.println("bad number of parameters. Usage: <ip> <port>");
+			return;
+		}
 
-        TelnetClient telnetClient = new TelnetClient("127.0.0.1", 23);
-        telnetClient.run(); // run telnet client
-    }
+		String ip = args[0];
+		int port = 23;
+		try {
+			port = Integer.parseInt(args[1]);
+		} catch (NumberFormatException e) {
+			System.err.println("port must be an integer");
+			return;
+		}
+
+		TelnetClient telnetClient = new TelnetClient(ip, port);
+		telnetClient.run();
+	}
 }
--- app/src/main/java/utb/fai/TelnetClient.java
+++ app/src/main/java/utb/fai/TelnetClient.java
@@ -2,25 +2,68 @@ package utb.fai;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class TelnetClient {
 
-    private String serverIp;
-    private int port;
+	private final String serverIp;
+	private final int port;
 
-    public TelnetClient(String serverIp, int port) {
-        this.serverIp = serverIp;
-        this.port = port;
-    }
+	public TelnetClient(String serverIp, int port) {
+		this.serverIp = serverIp;
+		this.port = port;
+	}
 
-    public void run() {
-        try {
-            Socket socket = new Socket(serverIp, port);
-            // Implementation of receiving and sending data
-            // Implement processing of input from the user and sending data to the server
-            // Implement response processing from the server and output to the console
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public void run() {
+		try (Socket socket = new Socket(serverIp, port)) {
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+
+			BufferedReader stdinReader = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader socketReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+			BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+
+			Thread readerThread = new Thread(() -> {
+				try {
+					while (true) {
+						if (in.available() > 0) {
+							String line = socketReader.readLine();
+							if (line == null)
+								break;
+							System.out.println(line);
+						} else {
+							Thread.sleep(20);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+
+			Thread writerThread = new Thread(() -> {
+				try {
+					String buffer;
+					while ((buffer = stdinReader.readLine()) != null) {
+						if (buffer.equals("/QUIT")) {
+							socket.close();
+							break;
+						}
+						socketWriter.write(buffer + "\r\n");
+						socketWriter.flush();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+
+			readerThread.start();
+			writerThread.start();
+
+			readerThread.join();
+			writerThread.join();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
